b1a0fa8ee911291ac43fa3b8ff12593af430ad1f
michelle <michellemuchoki8@gmail.com>
2025-05-27 16:43:20 +0300

    changes

--- models.py
+++ models.py
@@ -1,4 +1,15 @@
 from sqlalchemy.orm import DeclarativeBase
 # from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column,Integer,Text
+
 class Base(DeclarativeBase):
     pass
+
+class Driver(Base):
+    __tablename__="drivers"
+    id=Column(Integer(),primary_key=True)
+    name=Column(Text)
+    email=Column(Text)
+    gender=Column(Text)
+    id_number=Column(Integer)
+    vehicle_id=Column(Integer)
