e3d1b8ad5d078af38c1eda99f3cb5842071c7974
michelle <michellemuchoki8@gmail.com>
2025-05-27 20:52:22 +0300

    wip

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import DeclarativeBase
 # from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column,Integer,Text
+from sqlalchemy import Column,Integer,Text,Date
+from datetime import date
 
 class Base(DeclarativeBase):
     pass
@@ -13,3 +14,18 @@ class Driver(Base):
     gender=Column(Text)
     id_number=Column(Integer)
     vehicle_id=Column(Integer)
+    created_at = Column(Date, default=date.today)
+
+class Vehicle(Base):
+    __tablename__="vehicles"
+    id =Column(Integer(),primary_key=True)
+    No_plate=Column(Text)
+    driver_id =Column(Text)
+    route_id=Column(Text)
+    created_at = Column(Date, default=date.today)
+
+class Route(Base):
+    __tablename__="routes"
+    id=Column(Integer,primary_key=True)
+    vehicle_id =Column(Integer)
+    created_at = Column(Date, default=date.today)
